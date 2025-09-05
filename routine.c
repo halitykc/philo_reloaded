1a2eddc1eddf7bfc835811889a6a6635fe478dde
halitykc <87850150+halitykc@users.noreply.github.com>
2025-09-05 09:02:34 +0000

    norm

--- routine.c
+++ routine.c
@@ -2,6 +2,26 @@
 
 #include "philo.h"
 
+void    lock_eat(t_philo *philo)
+{
+    pthread_mutex_lock(&philo->rules->meal_lock);
+    print_status(philo, "is eating");
+    philo->last_meal = get_current_time();
+    philo->meal_eatten++;
+    pthread_mutex_unlock(&philo->rules->meal_lock);
+    ft_usleep(philo->rules->time_to_eat);
+    pthread_mutex_unlock(philo->r_fork);
+    pthread_mutex_unlock(philo->l_fork);
+    ft_usleep(1);
+}
+
+void    one_philo(t_philo *philo)
+{
+        pthread_mutex_lock(philo->l_fork);
+        print_status(philo, "has taken a fork");
+        ft_usleep(philo->rules->time_to_die);
+        pthread_mutex_unlock(philo->l_fork);
+}
 
 void    philo_eat(t_philo *philo)
 {
@@ -9,10 +29,7 @@ void    philo_eat(t_philo *philo)
         return;
     if  (philo->rules->total_philo == 1)
     {
-        pthread_mutex_lock(philo->l_fork);
-        print_status(philo, "has taken a fork");
-        ft_usleep(philo->rules->time_to_die);
-        pthread_mutex_unlock(philo->l_fork);
+        one_philo(philo);
         return;
     }
     if (philo->philo_id % 2 != 0)
@@ -29,18 +46,7 @@ void    philo_eat(t_philo *philo)
         pthread_mutex_lock(philo->l_fork);
         print_status(philo, "has taken a fork");
     }
-    pthread_mutex_lock(&philo->rules->meal_lock);
-    print_status(philo, "is eating");
-    philo->last_meal = get_current_time();
-    philo->meal_eatten++;
-    pthread_mutex_unlock(&philo->rules->meal_lock);
-    
-    ft_usleep(philo->rules->time_to_eat);
-
-
-    pthread_mutex_unlock(philo->r_fork);
-    pthread_mutex_unlock(philo->l_fork);
-    ft_usleep(1);
+    lock_eat(philo);
 }
 
 void philo_sleep(t_philo *philo)
